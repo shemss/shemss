0456d55fe303aa3cbb62a9897e3d3436b4beff98
Aleksey Gladysh <aleksey.gladysh@gmail.com>
2015-01-13 21:04:09 -0800

    Implement shard tabs

--- _assets/javascripts/bootstrap.js
+++ _assets/javascripts/bootstrap.js
@@ -4,7 +4,7 @@
 // require bootstrap/carousel
 //= require bootstrap/collapse
 // require bootstrap/dropdown
-// require bootstrap/tab
+//= require bootstrap/tab
 //= require bootstrap/transition
 // require bootstrap/scrollspy
 // require bootstrap/modal
